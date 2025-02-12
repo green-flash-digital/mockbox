1ff55470225e83978f908d1b9540e8a18a5296e2
Drew DeCarme <drewdecarme@gmail.com>
2025-02-12 14:15:46 -0500

    feat: Adds generic to properly type Pagination

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export type { Product, ProductCategory } from "../scripts/seed-products.js";
 export type { User } from "../scripts/seed-users.js";
 
-export type Pagination = {
+export type Pagination<T extends Record<string, unknown> = any> = {
   page: number;
   page_size: number;
-  data: any[];
+  data: T[];
   next: number | null;
   pages: number;
   count: number;
